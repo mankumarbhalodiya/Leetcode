2bac905cfb2b1514d9ac54e97f09bcd4ce11118a
Man Bhalodiya <manbhalodiya22@gmail.com>
2026-06-27 10:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.27%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        n = len(s)
+
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i == n:
+            return 0
+
+        sign = 1
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        result = 0
+        while i < n and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1
+
+        result *= sign
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+
+        return result
