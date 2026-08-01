388e84a7793b8870c8d284c69a2a275b72b70814
Man Bhalodiya <manbhalodiya22@gmail.com>
2026-08-01 09:48:05 +0530

    Time: 8 ms (45.55%) | Memory: 14.2 MB (48.35%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        if (s.size() < 2)
+            return "";
+
+        unordered_set<char> st(s.begin(), s.end());
+
+        for (int i = 0; i < s.size(); i++) {
+            if (st.count(tolower(s[i])) && st.count(toupper(s[i])))
+                continue;
+
+            string left = longestNiceSubstring(s.substr(0, i));
+            string right = longestNiceSubstring(s.substr(i + 1));
+
+            return left.size() >= right.size() ? left : right;
+        }
+
+        return s;
+    }
+};
